8805743f723507ca116c30c0f4edba573c487b58
Nahian <enderdreams@outlook.com>
2026-04-16 14:20:47 +0600

    fix: restore base path to /COA-A2/

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fsm/",
+  base: "/COA-A2/",
   plugins: [react(), tailwindcss()],
 });
